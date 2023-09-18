946489f1526e9cde9771e0bb9c6961950eeb7e6b
YunYu-FIsh <yunyu.fish@gmail.com>
2023-09-18 03:56:56 -0500

    Initial commit to week2 assignment

---
+++ CIS175_JavaII/Week2Demo/src/main/java/controller/biggerNum.java
@@ -0,0 +1,26 @@
+/**
+ * @author yunyu - yyu3@dmacc.edu
+ * CIS175 - Fall 2021
+ * Sep 18, 2023
+ */
+package controller;
+
+public class biggerNum {
+
+	public static void main(String[] args) {
+		
+		System.out.println(biggerNum(1,5));
+
+	}
+
+	public static boolean biggerNum(int i, int j) {
+		
+		if(i>j) {
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+
+}
